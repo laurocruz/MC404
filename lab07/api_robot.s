35a192a7249e0ed323f121594dc6a790fc549ffd
Lauro Cruz <laurocruzsouza@gmail.com>
2015-11-03 17:39:27 -0200

    storing callee-save registers

--- lab07/api_robot.s
+++ lab07/api_robot.s
@@ -12,6 +12,8 @@
 set_speed_motor:
     @ r0 <- speed 
     @ r1 <- motor id 
+    stmfd sp!, {r4-r11, lr} 
+
     cmp r1, #0          @ se r1 == 0, vai para funcao write_motor0
     beq write_motor0    @ caso contrario, vai para write_motor1
 
@@ -19,35 +21,46 @@ write_motor1:
     mov r7, #127
     svc 0x0
 
-    bx lr
+    ldmfd sp!, {r4-r11, lr}
+    mov pc, lr
 
 write_motor0:
     mov r7, #126
     svc 0x0
 
-    bx lr
+    ldmfd sp!, {r4-r11, lr}
+    mov pc, lr
 
 @@@
 set_speed_motors:
     @ r0 <- speed 0
     @ r1 <- speed 1
+    stmfd sp!, {r4-r11, lr} 
+
     mov r7, #124
     svc 0x0
 
-    bx lr
+    ldmfd sp!, {r4-r11, lr}
+    
+    mov pc, lr
 
 @@@
 read_sonar:
     @ r0 <- sonar id                    
     @ return (r0) <- 13-bit unsigned int
+    stmfd sp!, {r4-r11, lr}
+
     mov r7, #125 @ num for read_sonar
     svc 0x0
 
-    bx lr
+    ldmfd sp!, {r4-r11, lr}
+    mov pc, lr
 
 @@@
 read_sonars:
     @ r0 <- array of integers address
+    stmfd sp!, {r4-r11, lr}
+
     mov r7, #125 @ num for read_sonar
     mov r1, r0   @ saves array address in r1 and r2
     mov r2, r0
@@ -63,5 +76,6 @@ do_while:
 
     mov r0, r2        @ stores the array address again in r0
 
-    bx lr
+    ldmfd sp!, {r4-r11, lr}
+    mov pc, lr
 
